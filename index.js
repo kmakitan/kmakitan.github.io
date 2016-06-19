20a8c25d3931b19a792e3b5ef0cf42fcfdd67729
Kenzo Makitani <Kmakitan@uci.edu>
2016-06-19 00:23:01 -0700

    june 19 update

---
+++ index.js
@@ -0,0 +1,6 @@
+var img = document.getElementById('BG'); 
+//or however you get a handle to the IMG
+var bgWidth = img.clientWidth;
+var bgHeight = img.clientHeight;
+
+document.getElementById("navLine").style.width = bgWidth;
