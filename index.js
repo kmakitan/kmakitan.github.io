ca8b96b1df3887fb7da935f01176a0b0c2266071
Kenzo Makitani <Kmakitan@uci.edu>
2016-06-24 18:40:25 -0700

    projects progress

--- index.js
+++ index.js
@@ -1,6 +1,28 @@
-var img = document.getElementById('BG'); 
-//or however you get a handle to the IMG
-var bgWidth = img.clientWidth;
-var bgHeight = img.clientHeight;
+$(document).ready(function() {
+    $('a[href^="#"]').on('click', function(event) {
+        var target = $(this.getAttribute('href'));
+        if (target.length) {
+            event.preventDefault();
+            $('html, body').stop().animate({
+                scrollTop: target.offset().top + 1
+            }, 500);
+        }
+    });	
 
-document.getElementById("navLine").style.width = bgWidth;
+
+    //Scroll Highlight
+    $(window).scroll(function() {
+
+        var position = $(this).scrollTop();
+
+        $('.section').each(function() {
+            var target = $(this).offset().top;
+            var id = $(this).attr('id');
+
+            if (position >= target) {
+                $('#navUl > li > a').removeClass('active');
+                $('#navUl > li > a[href=\"#' + id + '\"]').addClass('active');
+            }
+        });
+    });
+});
