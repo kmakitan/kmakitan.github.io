247938d11adddbe3c9d23eda3e0a227b69549c51
Kenzo Makitani <kmakitan@stanford.edu>
2016-08-08 15:28:18 -0700

    Cleaned up code

--- main.js
+++ main.js
@@ -32,3 +32,16 @@ $(document).ready(function() {
         });
     });
 });
+/* Set the width of the side navigation to 250px and the left margin of the page content to 250px and add a black background color to body */
+function openNav() {
+    document.getElementById("navUl").style.width = "250px";
+    document.getElementById("site-wrap").style.marginLeft = "250px";
+    document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
+}
+
+/* Set the width of the side navigation to 0 and the left margin of the page content to 0, and the background color of body to white */
+function closeNav() {
+    document.getElementById("navUl").style.width = "0";
+    document.getElementById("site-wrap").style.marginLeft = "0";
+    document.body.style.backgroundColor = "white";
+}
